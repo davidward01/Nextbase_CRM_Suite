6f7023375ef50802328b8dcc374cd80b62ba04a8
davidward01 <davidward.info@gmail.com>
2022-01-25 23:24:23 -0500

    Initial commit

---
+++ src/test/java/com/nextbase/runners/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.nextbase.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt", //store failed scenario names into rerun.txt file
+                "json:target/cucumber.json" //generate json execution report to be used for html report
+        },
+        features = "src/test/resources/features",
+        glue = "com/nextbase/step_definitions",
+        dryRun = false    //false=> run the test. true => check for missing steps
+//        , tags = ""
+)
+public class CukesRunner {
+}
